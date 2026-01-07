bdd82171d83febb62f675a66d5dca734641b451f
Yusuf Sami <yusufsami4103@gmail.com>
2026-01-07 14:28:51 +0100

    Create Main.py

---
+++ src/Main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 14:34:48 2022
+
+@author: Original template by Rolf van Lieshout
+"""
+
+import Problem, Solution, Route
+from ALNS import ALNS
+import time
+
+# List of instances to solve
+instances = ["instances/c202C16.txt",
+             "instances/lc102.txt",
+             "instances/lc108.txt",
+             "instances/lc207.txt",
+             "instances/lr112.txt",
+             "instances/lr205.txt",
+             "instances/lrc104.txt",
+             "instances/lrc206.txt",
+             "instances/r102C18.txt",
+             "instances/rc204C16.txt"]
+
+# Number of destroy and repair operators
+nDestroyOps = 4
+nRepairOps = 3
+
+# Select the instance(s) to solve
+test = instances[6]
+
+# Solve the instance(s) and track the total runtime
+starttime = time.time()
+print(f"Solving instance {test}")
+problem = Problem.PDPTW.readInstance(test)
+print(problem)
+alns = ALNS(problem, nDestroyOps, nRepairOps)
+alns.execute()
+endtime = time.time()
+
+print(f"Total time: {endtime - starttime} seconds")
